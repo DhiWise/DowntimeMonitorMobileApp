ff54ea478028446bd18d67054e8f9d61364131ee
AshutoshDaveDhiWise <106240117+AshutoshDaveDhiWise@users.noreply.github.com>
2022-07-22 19:17:12 +0530

    Delete statusmobile-v1.0.1 directory

--- statusmobile-v1.0.1/lib/presentation/category_e_screen/binding/category_e_binding.dart
+++
@@ -1,9 +0,0 @@
-import '../controller/category_e_controller.dart';
-import 'package:get/get.dart';
-
-class CategoryEBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => CategoryEController());
-  }
-}
--- statusmobile-v1.0.1/lib/presentation/category_e_screen/models/category_e_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import '../../category_c_screen/models/category_c_item_model.dart';
-import 'category_item_model.dart';
-
-class CategoryEModel {
-  RxList<CategoryCItemModel> categoryItemList =
-      RxList.filled(0, CategoryCItemModel());
-}
